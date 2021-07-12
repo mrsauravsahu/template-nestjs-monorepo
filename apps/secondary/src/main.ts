832e8885fc441d7cb8d4273c47b7cb359202c247
Your Name <mrsauravsahu@outlook.com>
2021-07-12 13:36:02 +0530

    add secondary service to convert to monorepo

---
+++ apps/secondary/src/main.ts
@@ -0,0 +1,8 @@
+import { NestFactory } from '@nestjs/core';
+import { SecondaryModule } from './secondary.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(SecondaryModule);
+  await app.listen(3000);
+}
+bootstrap();
