915857fb9145c61e2b809d478951acf4127dd5bc
Saurav Sahu <mrsauravsahu@outlook.com>
2021-07-13 12:58:07 +0530

    add startup log to secondary service

--- apps/secondary/src/main.ts
+++ apps/secondary/src/main.ts
@@ -4,5 +4,7 @@ import { SecondaryModule } from './secondary.module';
 async function bootstrap() {
   const app = await NestFactory.create(SecondaryModule);
   await app.listen(3000);
+
+  console.log(`App running at ${app.getUrl()}`);
 }
 bootstrap();
